4a725d7e9cac4aeef0879f4f6441fa3679cc6eab
Ruben Manuel <rubensapalo97@gmail.com>
2025-04-11 23:40:45 +0100

    UserGetailsService

---
+++ src/main/java/com/rudev/dscommerce/DSCommerce/service/UserService.java
@@ -0,0 +1,42 @@
+package com.rudev.dscommerce.DSCommerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.rudev.dscommerce.DSCommerce.entities.Role;
+import com.rudev.dscommerce.DSCommerce.entities.User;
+import com.rudev.dscommerce.DSCommerce.projections.UserDetailsProjection;
+import com.rudev.dscommerce.DSCommerce.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+		if(result.isEmpty()) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		
+		for(UserDetailsProjection projection : result) {	
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		
+		return user;
+		
+	}
+	
+}
