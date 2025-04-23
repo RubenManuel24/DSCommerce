4c99a9fa18a455a03e684d2c39c61cb6e0be45ec
Ruben Manuel <rubensapalo97@gmail.com>
2025-04-24 00:18:35 +0100

    Deploy Ok

--- src/main/java/com/rudev/dscommerce/DSCommerce/DsCommerceApplication.java
+++ src/main/java/com/rudev/dscommerce/DSCommerce/DsCommerceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsCommerceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("🚀 Aplicação iniciando...");
 		SpringApplication.run(DsCommerceApplication.class, args);
 	}
 
